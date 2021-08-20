d1581620315eafd3935cf84702eecad80b1fec6f
jface <jface2038@gmail.com>
2021-08-20 14:50:37 +0800

    测试GitHub的分支管理功能

---
+++ src/main/java/com/test/day01/Demo04.java
@@ -0,0 +1,13 @@
+package com.test.day01;
+
+/**
+ * @Author Jface
+ * @Date 2021/8/20 14:49
+ * @Since version-1.0
+ * @Desc 测试GitHub的分支提交
+ */
+public class Demo04 {
+    public static void main(String[] args) {
+        System.out.println("测试GitHub的分支管理");
+    }
+}
