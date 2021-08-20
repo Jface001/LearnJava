328588d1fe6386bf4b34b4b771e1c3e7dab3f9b2
jface <jface2038@gmail.com>
2021-08-20 12:08:01 +0800

    first commit

---
+++ src/main/java/com/test/day01/Demo01.java
@@ -0,0 +1,15 @@
+package com.test.day01;
+
+/**
+ * @Author Jface
+ * @Date 2021/8/20 11:51
+ * @Since version-1.0
+ * @Desc
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 23; i++) {
+            System.out.println("这是第:"+i+"循环");
+        }
+    }
+}
---
+++ src/main/java/com/test/day01/Demo02JDBC.java
@@ -0,0 +1,27 @@
+package com.test.day01;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+/**
+ * @Author Jface
+ * @Date 2021/8/20 11:54
+ * @Since version-1.0
+ * @Desc 写一个JDBC连接本地MySQL, 查询数据
+ */
+public class Demo02JDBC {
+    public static void main(String[] args) {
+        //1.抽取变量
+        Connection conn = null;
+        PreparedStatement  stat = null;
+        ResultSet rs = null;
+
+        //2.创建客户端连接对象
+
+        //3.编译提交SQL语句
+
+        //4.解析结果集并打印输出
+
+    }
+}
